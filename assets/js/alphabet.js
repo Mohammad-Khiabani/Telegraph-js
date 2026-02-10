7db570d357bac95fbb8f2d334255776e7fd79c48
mohammad-khiabani <mhammad.khy@gmail.com>
2026-02-10 20:08:39 +0330

    feat: restructure alphabet and morse code definitions for clarity

--- assets/js/alphabet.js
+++ assets/js/alphabet.js
@@ -1,4 +1,5 @@
-export const alphabet = [
+// English alphabet
+const englishAlphabet = [
   "A",
   "B",
   "C",
@@ -26,7 +27,30 @@ export const alphabet = [
   "Y",
   "Z",
 ];
-export const morseCodes = [
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const puncations = [
+  `&`, // Ampersand
+  `'`, // Apostrophe
+  `@`, // At sign
+  `)`, // Bracket, close (parenthesis)
+  `(`, // Bracket, open (parenthesis)
+  `:`, // Colon
+  `,`, // Comma
+  `=`, // Equals sign
+  `!`, // Exclamation markNot in ITU-R recommendation
+  `.`, // Full-stop (period)
+  `-`, // Hyphen
+  `×`, // Multiplication sign (also x)
+  `%`, // Percentage
+  `+`, // Plus sign
+  `"`, // Quotation marks
+  `?`, // Question mark (query)
+  `/`, // Slash
+];
+
+// Morse alphabet
+export const morseCodeAlphabet = [
   ".-", //"A"
   "-...", //"B"
   "-.-.", //"C"
@@ -53,4 +77,44 @@ export const morseCodes = [
   ".--", //"X"
   "-.--", //"Y"
   "--..", //"Z"
+];
+
+const morseCodeDigits = [
+  "-----", //0
+  ".----", //1
+  "..---", //2
+  "...--", //3
+  "....-", //4
+  ".....", //5
+  "-....", //6
+  "--...", //7
+  "---..", //8
+  "----.", //9
+];
+
+const morseCodePuncations = [
+  ".-...", // &
+  ".----.", // '
+  ".--.-.", // @
+  "-.--.-", // )
+  "-.--.", // (
+  "---...", // :
+  "--..--", // ,
+  "-...-", // =
+  "-.-.--", // !
+  ".-.-.-", // .
+  "-....-", // -
+  "-..-", // ×
+  "----- -..-. -----", // % (literally 0/0)
+  ".-.-.", // +
+  ".-..-.", // "
+  "..--..", // ?
+  "-..-.", // /
+];
+
+export const allLetters = [...englishAlphabet, ...digits, ...puncations];
+export const allMorseCode = [
+  ...morseCodeAlphabet,
+  ...morseCodeDigits,
+  ...morseCodePuncations,
 ];
